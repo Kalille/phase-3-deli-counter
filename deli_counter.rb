6d9536f0f899cdd04371fcb5dee6d23aee4d0dfc
kalille Overton <>
2021-08-20 23:36:29 -0400

    commit tree 06fe7e58361a0788b73e4e4e8c399b6c18596e7e

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+require 'pry'
+def line(array)
+    if array.length == 0
+    puts "The line is currently empty."
+    else 
+        i=[]
+     array.each_with_index { |x, index|  i<< "#{index+1}. #{x}"}
+    
+     puts "The line is currently: #{i.join(" ")}"
+    end
+    
+end
+def take_a_number(array,name)
+    array<<name
+    puts "Welcome, #{name}. You are number #{array.length} in line."
+  
+end
+
+def now_serving(array)
+       
+  if array.length == 0
+     puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{array.first}."
+    array.shift
+  end
+     
+end
